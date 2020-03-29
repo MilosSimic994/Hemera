bf714472302556a631aa5b734baef3030fb02b5a
Milos Simic <milossimicc@gmail.com>
2020-03-29 15:04:38 +0200

    style

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,25 +3,37 @@ let footer = document.createElement('footer')
 let socDiv = document.createElement('div')
 
 let instaImg = document.createElement('img')
-instaImg.src = '../assets/images/insta_logo2.jpg'
+instaImg.src = '../assets/images/insta1.png'
 
 let instaLink = document.createElement('a')
 instaLink.href = 'https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/learn/lecture/15853252#overview'
 
-let contactDiv = document.createElement('div')
-contactDiv.className = 'footer-contact'
+let mailImg = document.createElement('img')
+mailImg.src = '../assets/images/mail-icon.png'
 
-let mailPar = document.createElement('p')
-mailPar.innerText = 'milossimic@gmail.com'
+let mailLink = document.createElement('a')
+mailLink.href = 'https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/learn/lecture/15853252#overview'
 
-let numberPar = document.createElement('p')
-numberPar.innerText = '0653554317'
+mailLink.append(mailImg)
 
-contactDiv.append(mailPar, numberPar)
+let phoneImg = document.createElement('i')
+phoneImg.className = 'fas fa-user' 
+
+
+// let contactDiv = document.createElement('div')
+// contactDiv.className = 'footer-contact'
+
+// let mailPar = document.createElement('p')
+// mailPar.innerText = 'milossimic@gmail.com'
+
+// let numberPar = document.createElement('p')
+// numberPar.innerText = '0653554317'
+
+// contactDiv.append(mailPar, numberPar)
 
 instaLink.appendChild(instaImg)
 
-socDiv.append(instaLink, contactDiv)
+ socDiv.append(instaLink, mailLink, phoneImg)
 
 footer.appendChild(socDiv)
 
